57f9d4b093d1541c25170c0e695a44e6772d61ca
Mauricio Aguilera <artuaguila@gmail.com>
2025-02-07 16:35:19 -0600

    First commit

---
+++ weather_app/weather_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather_app
+//
+//  Created by Diplomado on 07/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
