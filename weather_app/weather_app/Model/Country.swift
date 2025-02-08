fb4e69ef2bdbd9c5a7b55333d8b875f474262d54
Mauricio Aguilera <artuaguila@gmail.com>
2025-02-07 20:19:54 -0600

    Add Country structure and ModelData

--- weather_app/weather_app/Model/Country.swift
+++ weather_app/weather_app/Model/Country.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Country : Codable, Hashable, Identifiable{
+    var id : Int
+    var name: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name = "nombre"
+    }
+}
